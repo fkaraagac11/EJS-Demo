cfe1863066003be17682a90890e9db3f609bf053
fkaraagac11 <fehmi11.qa@gmail.com>
2020-07-16 23:28:39 -0400

    updated

--- app.js
+++ app.js
@@ -8,12 +8,27 @@ app.get("/", (req, res) => {
     res.render("home.ejs");
 });
 
+app.get("/posts", function (req, res) {
+    const posts = [
+        { title: "Post-1", author: "Kafka" },
+        { title: "Post-2", author: "Steve Hawkings" },
+        { title: "Post-3", author: "Benjamin" },
+    ];
+
+    res.render("posts.ejs", { posts: posts });
+});
+
+{
+    let y = 5;
+}
+
+console.log(y);
+
 app.get("/fall/:thing", (req, res) => {
     const thing = req.params.thing;
-    const pass = { thing: thing };
-    res.render("love.ejs", pass);
+    res.render("love.ejs", { thingVar: thing });
 });
 
 app.listen(3000, () => {
-    console.log("Server is listening for EJS-Embedded JS");
+    console.log("Server is listening for EJS");
 });
